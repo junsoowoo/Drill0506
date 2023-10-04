1bde7592739b0a3504ec7097550032290ee92e5c
Junsoo Han <hanjs991124@gmail.com>
2023-10-04 15:26:04 +0900

    기본 refactoring 완료.(함수 구조화)

--- Drill05.py
+++ Drill05.py
@@ -1,10 +1,12 @@
 from pico2d import *
 
 TUK_WIDTH, TUK_HEIGHT = 1280, 1024
-open_canvas(TUK_WIDTH, TUK_HEIGHT)
 
-TUK_ground = load_image('TUK_GROUND.png')
-character = load_image('animation_sheet.png')
+
+def road_resources():
+    global TUK_ground, character
+    TUK_ground = load_image('TUK_GROUND.png')
+    character = load_image('animation_sheet.png')
 
 
 def handle_events():
@@ -21,23 +23,33 @@ def handle_events():
     pass
 
 
+def reset_world():
+    global running, x, y, frame
+    running = True
+    x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2
+    frame = 0
 
-running = True
-x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2
-frame = 0
-hide_cursor()
 
-while running:
+def render_world():
     clear_canvas()
     TUK_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
     character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
     update_canvas()
+
+
+def update_world():
+    global frame
     frame = (frame + 1) % 8
 
+
+open_canvas(TUK_WIDTH, TUK_HEIGHT)
+hide_cursor()
+road_resources()
+reset_world()
+
+while running:
+    render_world()
     handle_events()
+    update_world()
 
 close_canvas()
-
-
-
-
