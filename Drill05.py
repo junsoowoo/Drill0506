2fd67a3e045844424d21ec02b0a19ffebfb16bcf
Junsoo Han <hanjs991124@gmail.com>
2023-10-04 16:24:28 +0900

    랜덤 위치 한 개로 이동 완료. 방향에 대한 조정도 action변수를 이용해 처리 완료

--- Drill05.py
+++ Drill05.py
@@ -8,10 +8,9 @@ def road_resources():
     global TUK_ground, character
     global arrow
 
-    arrow= load_image('hand_arrow.png')
+    arrow = load_image('hand_arrow.png')
     TUK_ground = load_image('TUK_GROUND.png')
     character = load_image('animation_sheet.png')
-
 
 
 def handle_events():
@@ -28,17 +27,21 @@ def handle_events():
 
 def reset_world():
     global running, cx, cy, frame
-    global hx, hy, sx,sy, t, action
+    global hx, hy, sx, sy, t, action
     running = True
     cx, cy = TUK_WIDTH // 2, TUK_HEIGHT // 2
     frame = 0
     action = 3
 
+    set_new_target_arrow()
 
-    sx, sy = cx, cy #p1: 시작점
-    hx, hy = random.randint(0, TUK_WIDTH - 1), random.randint(0, TUK_HEIGHT - 1) #p2 : 끝점
+
+def set_new_target_arrow():
+    global sx, sy, hx, hy, t
+    sx, sy = cx, cy  # p1: 시작점
+    hx, hy = random.randint(0, TUK_WIDTH - 1), random.randint(0, TUK_HEIGHT - 1)  # p2 : 끝점
     # hx, hy = 50, 50
-    t= 0.0
+    t = 0.0
 
 
 def render_world():
@@ -51,17 +54,19 @@ def render_world():
 
 def update_world():
     global frame
-    global cx, cy ,t
+    global cx, cy, t
     global action
 
     frame = (frame + 1) % 8
     action = 1 if cx < hx else 0
 
     if t <= 1.0:
-        cx = (1-t)*sx + t*hx
-        cy = (1-t)*sy + t*hy
-        t +=0.001
-
+        cx = (1 - t) * sx + t * hx
+        cy = (1 - t) * sy + t * hy
+        t += 0.001
+    else:
+        cx, cy= hx, hy #캐릭터 위치를 목적지 위치와 정확히 일치시킴.
+        set_new_target_arrow()
 
 open_canvas(TUK_WIDTH, TUK_HEIGHT)
 hide_cursor()
